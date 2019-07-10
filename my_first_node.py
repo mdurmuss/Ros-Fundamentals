8f712c1e89a3e76b6d864acb51eb735abff95840
mustafa <mustafa-durmuss@outlook.com>
2019-07-10 11:00:13 +0300

    first-node

--- my_first_node.py
+++ my_first_node.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy # import python library in ros
 
-if __name__ == '__main__':
-	rospy.init_node('my_first_python_node')
-	rospy.loginfo("This node has been started")
+if __name__ == '__main__': 
+	rospy.init_node('my_first_python_node') # create first node with its name.
+	rospy.loginfo("This node has been started") # write a message to log
 
-	rate = rospy.Rate(10)
+	rate = rospy.Rate(10) # frequency
 
-	while not rospy.is_shutdown():
+	while not rospy.is_shutdown(): # until rospy is still working
 		rospy.loginfo("Hello")
 		rate.sleep()
