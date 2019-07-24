5a808245d735578cdbf33d2cf0239b137ba95535
mustafa <mustafa-durmuss@outlook.com>
2019-07-24 11:17:40 +0300

    some changes

--- Example2/number_counter.py
+++ Example2/number_counter.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+import rospy
+from std_msgs.msg import Int64
+
 NODE_NAME = "numberCounter"
 SUB_TOPIC_NAME = "/number"
 PUB_TOPIC_NAME = "/number_count"
 
-import rospy
-from std_msgs.msg import Int64
-# import related libs
 counter = 0
 
+
 def myfunction(message):
+    # here there is a function(myfunction)
+    # this function will be called when that topic will send a message.
+    # after taking that message that function will show it.
+    # and will add it to counter and publish it.
     rospy.loginfo("Message has just received!")
     rospy.loginfo(message)
     global counter
@@ -18,17 +23,15 @@ def myfunction(message):
     new_msg = Int64()
     new_msg.data = counter
     pub.publish(new_msg)
-    
+
+
 if __name__ == "__main__":
     rospy.init_node(NODE_NAME)
-    subscriber = rospy.Subscriber(SUB_TOPIC_NAME,Int64,myfunction) # create a subscriber
-    # here there is a function(myfunction)
-    # this function will be called when that topic will send a message.
-    # after taking that message that function will show it.
-    # and will add it to counter and publish it.
-    pub = rospy.Publisher(PUB_TOPIC_NAME,Int64,queue_size=10) # create a publisher
+    # that node is a subscriber to "/number" topic
+    # and a publisher of "/number_count" topic.
+    subscriber = rospy.Subscriber(SUB_TOPIC_NAME, Int64, myfunction)
+    # create a subscriber
+    pub = rospy.Publisher(PUB_TOPIC_NAME, Int64, queue_size=10)
+    # create a publisher
     rate = rospy.Rate(2)
-    
     rospy.spin()
-
-# that node is a subscriber to "/number" topic and a publisher of "/number_count" topic.
--- Example2/number_publisher.py
+++ Example2/number_publisher.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-NODE_NAME = "NumberNode"
-TOPIC_NAME = "/number"
+
 import rospy
 from std_msgs.msg import Int64
 
-if __name__ == "__main__":
-    rospy.init_node(NODE_NAME,anonymous=True)
 
-    publisher = rospy.Publisher(TOPIC_NAME,Int64,queue_size=10) 
+NODE_NAME = "NumberNode"
+TOPIC_NAME = "/number"
+
+if __name__ == "__main__":
+    rospy.init_node(NODE_NAME, anonymous=True)
+
+    publisher = rospy.Publisher(TOPIC_NAME, Int64, queue_size=10)
     # number is the topic name.
     # Int64 is the type of message of that topic will publish
     rate = rospy.Rate(2)
@@ -19,5 +22,5 @@ if __name__ == "__main__":
         publisher.publish(message)
         # lets publish it
         rate.sleep()
-    
+
     rospy.loginfo("Node has stopped!")
