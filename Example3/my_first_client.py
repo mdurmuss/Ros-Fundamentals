77fb8fb30a927be0cb07dff95285a367803f7ea3
mustafa <mustafa-durmuss@outlook.com>
2019-07-24 11:24:17 +0300

    added pep8

--- Example3/my_first_client.py
+++ Example3/my_first_client.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 
-NODE_NAME = "add_two_ints_client"
-SERVICE_NAME = "/add_two_ints"
 import rospy
 from rospy_tutorials.srv import AddTwoInts
 
+NODE_NAME = "add_two_ints_client"
+SERVICE_NAME = "/add_two_ints"
+
+
 if __name__ == "__main__":
     rospy.init_node(NODE_NAME)
-    
-    rospy.wait_for_service(SERVICE_NAME) # wait until the service is available
+    rospy.wait_for_service(SERVICE_NAME)  # wait until the service is available
 
     # after service is available, lets do our job
     # first write a try/except block for any fault about service
 
     for i in range(10):
         try:
-            add_two_ints = rospy.ServiceProxy(SERVICE_NAME,AddTwoInts) # client created
-            response = add_two_ints(i,i+3)  # response from service
+            add_two_ints = rospy.ServiceProxy(SERVICE_NAME, AddTwoInts)
+            # client created
+            response = add_two_ints(i, i+3)
+            # response from service
             rospy.loginfo("Sum is : "+str(response))
         except rospy.ServiceException as e:
             rospy.logwarn("Service failed because of : "+str(e))
-
-
-
-
-
-
-
--- Example3/my_first_service.py
+++ Example3/my_first_service.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
-NODE_NAME = "add_two_integer"
-SERVICE_NAME = "/add_two_ints"
-
-def handle_add_two_ints(req): # req is the returning variable from server
-    result = req.a + req.b 
-    rospy.loginfo("Sum of ( " + str(req.a)+ " and "+ str(req.b)+" ) is "+str(result))
-    return result # result will send to client
-
 import rospy
 from rospy_tutorials.srv import AddTwoInts
 
+NODE_NAME = "add_two_integer"
+SERVICE_NAME = "/add_two_ints"
+
+
+def handle_add_two_ints(req):  # req is the returning variable from server
+    result = req.a + req.b
+    rospy.loginfo("Sum of ( " + str(req.a) + " and " + str(req.b) +")
+    is "+str(result))
+    return result  # result will send to client
+
 if __name__ == "__main__":
-    rospy.init_node(NODE_NAME) # create server node
+    rospy.init_node(NODE_NAME)  # create server node
     rospy.loginfo("Add two integer server node has created!")
 
-    service = rospy.Service(SERVICE_NAME,AddTwoInts,handle_add_two_ints) # create service
+    service=rospy.Service(SERVICE_NAME, AddTwoInts, handle_add_two_ints)
     rospy.loginfo("Service server has been started!")
 
-    rospy.spin() # that make program runs until any interrupt
+    rospy.spin()  # that make program runs until any interrupt
+    
